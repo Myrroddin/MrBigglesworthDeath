3d9258908be6ae2ca8d79a462a78f29a3fbc1fbe
myrroddin <psvander@gmail.com>
2020-01-19 16:51:58 +0000

    - Rework localization code to be compatible with Curseforge localization system - Thanks to Shiromar for reporting the translation bug

--- MrBigglesworthDeath.lua
+++ MrBigglesworthDeath.lua
@@ -1,29 +1,59 @@
 -- Addon Name  : MrBigglesworthDeath
 -- Notes:      : Displays who killed Mr Bigglesworth, Kul'Thuzad's cat in Naxxramas to chat frame 1
 -- and plays ominous thunder in case the player missed chat.
--- Date: $Date$
+-- Date: @project-date-iso@
 
-local locales = {
-	enUS = {
-		["DEATH_MESSAGE"] = " %s killed %s, May he Rest In Peace.",
-	},
-	deDE = --@localization(locale="deDE", format="lua_table")@
-	frFR = --@localization(locale="frFR", format="lua_table")@
-	esES = --@localization(locale="esES", format="lua_table")@
-	esMX = --@localization(locale="esES", format="lua_table")@
-	itIT = --@localization(locale="itIT", format="lua_table")@
-	koKR = --@localization(locale="koKR", format="lua_table")@
-	ptBR = --@localization(locale="ptBR", format="lua_table")@
-	ruRU = --@localization(locale="ruRU", format="lua_table")@
-	zhCN = --@localization(locale="zhCN", format="lua_table")@
-	zhTW = --@localization(locale="zhTW", format="lua_table")@
-}
-
-local L = setmetatable(locales[GetLocale()] or locales.enUS, {__index = function(t, k)
-	local v = rawget(locales.enUS, k) or k
+-- localize the addon. we don't need to specify enUS as that is the default
+local L = setmetatable({}, {__index = function(t, k)
+	local v = tostring(k)
 	rawset(t, k, v)
 	return v
 end})
+
+if GetLocale() == "deDE" then
+	--@localization(locale="deDE", format="lua_additive_table")@
+	return
+end
+
+if GetLocale() == "esES" or GetLocale() == "esMX" then
+	--@localization(locale="esES", format="lua_additive_table")@
+	return
+end
+
+if GetLocale() == "frFR" then
+	--@localization(locale="frFR", format="lua_additive_table")@
+	return
+end
+
+if GetLocale() == "itIT" then
+	--@localization(locale="itIT", format="lua_additive_table")@
+	return
+end
+
+if GetLocale() == "koKR" then
+	--@localization(locale="koKR", format="lua_additive_table")@
+	return
+end
+
+if GetLocale() == "ptBR" then
+	--@localization(locale="ptBR", format="lua_additive_table")@
+	return
+end
+
+if GetLocale() == "ruRU" then
+	--@localization(locale="ruRU", format="lua_additive_table")@
+	return
+end
+
+if GetLocale() == "zhCN" then
+	--@localization(locale="zhCN", format="lua_additive_table")@
+	return
+end
+
+if GetLocale() == "zhTW" then
+	--@localization(locale="zhTW", format="lua_additive_table")@
+	return
+end
 
 local spelldamage = {
 	["SPELL_DAMAGE"] = true,
@@ -55,7 +85,7 @@ function f:OnEvent(event, ...)
 	if overkill > 0 then
 		local npcID = select(6, strsplit("-", destGUID))
 		if tonumber(npcID) == 16998 then
-			SendChatMessage(format(L.DEATH_MESSAGE, sourceName, destName), IsInRaid() and "RAID" or IsInGroup() and "PARTY" or "SAY")
+			SendChatMessage(format(L["%s killed %s, May he Rest In Peace."], sourceName, destName), IsInRaid() and "RAID" or IsInGroup() and "PARTY" or "SAY")
 			PlaySoundFile("Interface/AddOns/MrBigglesworthDeath/Sounds/thunder.ogg", "Master")
 			f:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 		end
