9f60ec8b9943714d7b21427eaacd172af0db9624
Myrroddin <psvander@gmail.com>
2026-06-06 07:10:29 -0600

    [Mists] Bump ToC for patch 5.5.4

--- MrBigglesworthDeath.lua
+++ MrBigglesworthDeath.lua
@@ -58,7 +58,7 @@ end
 
 local NAXXRAMAS_ID = 533
 local MR_BIGGLESWORTH_ID = 16998
-local isRetail = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
+local SOUND_FILE = "Interface/AddOns/MrBigglesworthDeath/Media/Sounds/thunder.ogg"
 
 ------------------------------------------------------------
 -- State
@@ -67,32 +67,16 @@ local isRetail = WOW_PROJECT_ID == WOW_PROJECT_MAINLINE
 local frame = CreateFrame("Frame")
 
 ------------------------------------------------------------
--- Classic CLEU control
+-- PARTY_KILL control
 ------------------------------------------------------------
 
-local function EnableCLEU()
-	if not frame:IsEventRegistered("COMBAT_LOG_EVENT_UNFILTERED") then
-		frame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-	end
-end
-
-local function DisableCLEU()
-	if frame:IsEventRegistered("COMBAT_LOG_EVENT_UNFILTERED") then
-		frame:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-	end
-end
-
-------------------------------------------------------------
--- Retail PARTY_KILL control
-------------------------------------------------------------
-
-local function EnablePK()
+local function EnablePartyKill()
 	if not frame:IsEventRegistered("PARTY_KILL") then
 		frame:RegisterEvent("PARTY_KILL")
 	end
 end
 
-local function DisablePK()
+local function DisablePartyKill()
 	if frame:IsEventRegistered("PARTY_KILL") then
 		frame:UnregisterEvent("PARTY_KILL")
 	end
@@ -103,34 +87,30 @@ end
 ------------------------------------------------------------
 
 local function CheckInstance()
-
 	local _, _, _, _, _, _, _, instanceID = GetInstanceInfo()
-	if not instanceID then return end
 
 	if instanceID == NAXXRAMAS_ID then
-		if isRetail then
-			EnablePK()
-		else
-			EnableCLEU()
-		end
+		EnablePartyKill()
 	else
-		if isRetail then
-			DisablePK()
-		else
-			DisableCLEU()
-		end
+		DisablePartyKill()
 	end
-
 end
 
 ------------------------------------------------------------
--- Retail kill handler
+-- Kill handling
 ------------------------------------------------------------
 
-local function HandlePartyKill(_, attackerGUID, targetGUID)
+---@param guid string
+---@return number?
+local function GetNPCIDFromGUID(guid)
+	return tonumber(guid:match("-(%d+)-"))
+end
 
-	local npcID = tonumber(targetGUID:match("-(%d+)-"))
-	if npcID ~= MR_BIGGLESWORTH_ID then return	end
+---@param attackerGUID string
+---@param targetGUID string
+local function HandlePartyKill(attackerGUID, targetGUID)
+	local npcID = GetNPCIDFromGUID(targetGUID)
+	if npcID ~= MR_BIGGLESWORTH_ID then return end
 
 	local killer = UnitNameFromGUID(attackerGUID) or UNKNOWN
 	local destName = UnitNameFromGUID(targetGUID) or "Mr. Bigglesworth"
@@ -143,79 +123,25 @@ local function HandlePartyKill(_, attackerGUID, targetGUID)
 		format(L["%s killed %s, May he Rest In Peace."], killer, destName), channel
 	)
 
-	PlaySoundFile(
-		"Interface/AddOns/MrBigglesworthDeath/Media/Sounds/thunder.ogg", "Master"
-	)
+	PlaySoundFile(SOUND_FILE, "Master")
 
-	DisablePK()
-
-end
-
-------------------------------------------------------------
--- Classic combat log handler
-------------------------------------------------------------
-
-local function HandleCombatLog()
-
-	local _, subevent, _, _, sourceName, _, _, destGUID, destName = CombatLogGetCurrentEventInfo()
-
-	if subevent ~= "SPELL_DAMAGE"
-		and subevent ~= "SPELL_PERIODIC_DAMAGE"
-		and subevent ~= "RANGE_DAMAGE"
-		and subevent ~= "SWING_DAMAGE"
-		and subevent ~= "ENVIRONMENTAL_DAMAGE" then
-	return	end
-
-	local npcID = destGUID and tonumber(destGUID:match("^Creature%-%d+%-%d+%-%d+%-(%d+)%-%d+$"))
-	if npcID ~= MR_BIGGLESWORTH_ID then
-		return
-	end
-
-	local overkill
-
-	if subevent == "SWING_DAMAGE" then
-		overkill = select(13, CombatLogGetCurrentEventInfo())
-	else
-		overkill = select(16, CombatLogGetCurrentEventInfo())
-	end
-
-	if not (overkill and overkill > 0) then
-		return
-	end
-
-	local channel = IsInRaid() and "RAID"
-	or IsInGroup() and "PARTY"
-	or "SAY"
-
-	C_ChatInfo.SendChatMessage(
-		format(L["%s killed %s, May he Rest In Peace."], sourceName, destName), channel
-	)
-
-	PlaySoundFile(
-		"Interface/AddOns/MrBigglesworthDeath/Media/Sounds/thunder.ogg", "Master"
-	)
-
-	DisableCLEU()
-
+	DisablePartyKill()
 end
 
 ------------------------------------------------------------
 -- Event dispatcher
 ------------------------------------------------------------
 
-frame:SetScript("OnEvent", function(self, event, ...)
-
+frame:SetScript("OnEvent", function(_, event, ...)
 	if event == "PLAYER_ENTERING_WORLD" or event == "ZONE_CHANGED_NEW_AREA" then
 		CheckInstance()
 		return
 	end
 
-	if isRetail and event == "PARTY_KILL" then
-		HandlePartyKill(event, ...)
-	elseif not isRetail and event == "COMBAT_LOG_EVENT_UNFILTERED" then
-		HandleCombatLog()
+	if event == "PARTY_KILL" then
+		local attackerGUID, targetGUID = ...
+		HandlePartyKill(attackerGUID, targetGUID)
 	end
-
 end)
 
 ------------------------------------------------------------
